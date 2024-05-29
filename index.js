efdfe081f9524ff8732fff408550b89169226102
vikasavchuk <s.vika02030@gmail.com>
2024-05-29 16:36:47 +0300

    project

---
+++ index.js
@@ -0,0 +1,43 @@
+import { program } from "commander";
+import { listContacts, removeContact, getContactById, addContact } from "./contacts.js";
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse();
+
+const options = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await listContacts();
+      console.table(contacts);
+      break;
+
+    case "get":
+      const searchContact = await getContactById(id);
+      console.log(searchContact);
+      break;
+
+    case "add":
+      const newContact = await addContact(name, email, phone)
+       console.log(newContact);
+      break;
+
+    case "remove":
+      const deletedContact = await removeContact(id);
+      console.log(deletedContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+
+invokeAction(options).catch(error => console.error(error));
